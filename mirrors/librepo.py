f7ff061be719ea1270289728cfa00bbe13af5dfe
Ethan House <ewhhouse@gmail.com>
2014-08-10 01:50:47 -0400

    Added config value checking.
    Basic value checking and sanity checks are made.
    Writing a config out should be as simple as offloading a dict.

--- mirrors/librepo.py
+++ mirrors/librepo.py
@@ -6,30 +6,61 @@ class repo:
         Repository Object.
         Gets stored in RepoManager
         :param name: string name of repo
-        :param config: dict of configuration values
+        :param config: dict of configuration
         """
-        # Contains config options
+        # running config options
         self._configOptions = config
 
         ### Config Settings
         # Repo Name
         self.name = name
+
         # rsync source url
-        self.source = self._configOptions['url']
+        if('source' in self._configOptions):
+            self.source = self._configOptions['source']
+        else:
+            raise ValueError('Config Sytax Error, Source Required in {0}.'.format(self.name),)
+
         # rsync args
-        self.args = self._configOptions['rsync_args']
+        if('rsync_args' in self._configOptions):
+            self.args = self._configOptions['rsync_args']
+        else:
+            raise ValueError('Config Sytax Error, rsync Args Required in {0}.'.format(self.name),)
+
         # rsync file destination
-        self.destination = self._configOptions['destination']
+        if('destination' in self._configOptions):
+            self.destination = self._configOptions['destination']
+        else:
+            self.destination = "./dist/"
+
         # Priority Queue Weight, value between -10 and 10
-        self.weight = self._configOptions['weight']
+        if('weight' in self._configOptions):
+            self.weight = self._configOptions['weight']
+        else:
+            self.weight = 0
+
         # Delay before next run is aloud to start
-        self.delayTime = self._configOptions['finish_offset']
+        if('finish_offset' in self._configOptions):
+            self.delayTime = self._configOptions['finish_offset']
+            self.strictTime = None
         # list of hours sync must happen on
-        #self.scrictTime = self._configOptions['hourly_run']
+        elif('hourly_run' in self._configOptions):
+            self.strictTime = self._configOptions['hourly_run']
+            self.delayTime = None
+        else:
+            raise ValueError('Config Sytax Error, either finish_offset or hourly_run required in {0}'.format(self.name))
+
         # Pre-bash Script
-        self.preScript = self._configOptions['pre_command']
+        if('pre_command' in self._configOptions):
+            self.preScript = self._configOptions['pre_command']
+        else:
+            self.preScript = None
+
         # Post-Bash Script
-        self.postScript = self._configOptions['post_command']
+        if('post_command' in self._configOptions):
+            self.postScript = self._configOptions['post_command']
+        else:
+            self.postScript = None
 
         ### Job Stats
         # Start time of last sucessful run
@@ -39,11 +70,41 @@ class repo:
         # Repo Avg run length
         self.runAvg = None
 
-    def returnConfig():
+        ### Function Calls
+        self.__writeConfig()
+
+    def __writeConfig(self):
+        """
+        Writes running config to stored config.
+        Required before syncing to disk
+        """
+        if self.source:
+            self._configOptions['source'] = self.source
+        if self.args:
+            self._configOptions['rsync_args'] = self.args
+        if self.destination:
+            self._configOptions['destination'] = self.destination
+        if self.weight:
+            self._configOptions['weight'] = self.weight
+        if self.delayTime:
+            self._configOptions['finish_offset'] = self.delayTime
+        if self.strictTime:
+            self._configOptions['hourly_run'] = self.strictTime
+        if self.preScript:
+            self._configOptions['pre_command'] = self.preScripa
+        else:
+            self._configOptions['pre_command'] = None
+        if self.postScript:
+            self._configOptions['post_command'] = self.postScript
+        else:
+            self._configOptions['post_command'] = None
+
+    def returnConfig(self):
         """
         Returns running config.
         :return dict: Dict of running config
         """
+        self.__writeConfig()
         return self._configOptions
 
 
